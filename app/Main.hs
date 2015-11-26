32f0bacd3038dc36e27b9b44f2d29f2c5fcff9f2
yamafaktory <yamafaktory@gmail.com>
2015-11-26 23:07:41 +0100

    Pushed utils as a module, fixed rendering.

--- app/Main.hs
+++ app/Main.hs
@@ -2,44 +2,19 @@
 
 module Main where
 
-import qualified Control.Foldl     as Fold
-import qualified Data.Foldable     as F
-import qualified Data.Text.IO      as TIO
 import qualified Data.Text         as T
-import           Text.HTML.TagSoup
+import qualified Data.Foldable     as F
 import           Turtle
+import           Utils             as U
 
-streamToList :: Shell a -> IO [a]
-streamToList stream = fold stream Fold.list
-
-countStream :: MonadIO io => Shell a -> io Int
-countStream stream = fold stream Fold.length
-
-fileStream :: Shell Turtle.FilePath
-fileStream = find (has ".html") "."
-
-test :: Monad m => m Turtle.FilePath -> m Text
-test file = do
-  Right a <- fmap toText file
-  return a
-
-fileList :: IO [Turtle.FilePath]
-fileList = streamToList fileStream
-
-parseHtmlFile :: Prelude.FilePath -> IO [Tag Text]
-parseHtmlFile file = parseTags <$> TIO.readFile file
-
-extractAttrs :: [Tag Text] -> [Text]
-extractAttrs = filter (/= "") . map (fromAttrib "data-e2e") . filter isTagOpen
-
-report :: Prelude.FilePath -> IO [Text]
-report file = extractAttrs <$> (parseHtmlFile file)
-
+main :: IO ()
 main = do
-  files <- fmap test fileList
-  print "------------------"
-  F.forM_ files $ \f -> do
-    print f
-    r <- report $ T.unpack $ f
-    print r
-    print "-----"
+  files <- fmap U.filePathToText U.fileList
+  let ret = "\n----------\n"
+  echo $ ret
+  F.forM_ files $ \file -> do
+    echo $ file <> "\n"
+    result <- U.report $ T.unpack $ file
+    F.forM_ result $ \tag -> do
+      echo $ "-> " <> tag
+    echo $ ret
