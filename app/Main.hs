53c910b9728b34ba129c9d6f0a1a160e510bc33b
yamafaktory <yamafaktory@gmail.com>
2015-11-27 00:21:28 +0100

    Refactored code.

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,20 @@
 
 module Main where
 
-import qualified Data.Text         as T
-import qualified Data.Foldable     as F
+import qualified Data.Foldable as F
+import qualified Data.Text     as T
 import           Turtle
-import           Utils             as U
+
+import           Utils         as U
 
 main :: IO ()
 main = do
-  files <- fmap U.filePathToText U.fileList
+  files <- U.filePathToText <$> U.fileList
   let ret = "\n----------\n"
   echo $ ret
   F.forM_ files $ \file -> do
-    echo $ file <> "\n"
+    echo $ format (w%"\n") file
     result <- U.report $ T.unpack $ file
     F.forM_ result $ \tag -> do
-      echo $ "-> " <> tag
+      echo $ format ("-> "%s) tag
     echo $ ret
