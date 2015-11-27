0f5f6ef90bf4e31cd3b11585f343be9786288029
yamafaktory <davy@qipp.com>
2015-11-27 16:05:43 +0100

    Updated output rendering.

--- app/Main.hs
+++ app/Main.hs
@@ -12,16 +12,20 @@ main :: IO ()
 main = do
   echo $ "Please provide the path to the HTML files:"
   Just filesPath <- readline
+  let line = T.replicate 60 $ T.pack "-"
+  let ret = T.pack "\n"
   let textFilesPath = fromText filesPath
+
   if T.length filesPath > 0 then do
+    totalFiles <- countStream (fileStream textFilesPath)
+    echo $ line <> ret <> format (d%" file(s) scanned") totalFiles
     files <- U.filePathToText <$> U.fileList textFilesPath
-    let ret = T.replicate 30 $ T.pack "- "
-    echo $ ret
+    echo $ line
     F.forM_ files $ \file -> do
-      echo $ format (w%"\n") file
+      echo $ format (w%"") file <> ret
       result <- U.report $ T.unpack $ file
       echo $ format ("Found "%d%" data attribute(s)") (length result)
       F.forM_ result $ \tag -> do
         echo $ format ("-> "%s) tag
-      echo $ ret
+      echo $ line
     else echo "Empty path - exit"
